00ab8e23c3694b6d8f498271b315c8faa780099f
Thacher <thacher@mac.com>
2017-10-11 13:00:04 -0400

    Added some code to fix the origin

--- app/src/main/java/com/thachershields/hw4/MainActivity.java
+++ app/src/main/java/com/thachershields/hw4/MainActivity.java
@@ -2,8 +2,11 @@ package com.thachershields.hw4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
